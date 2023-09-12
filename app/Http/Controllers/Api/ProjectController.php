e8e400a4dce71527336da825a27b151fe9a70670
cloudhuh <claudiu.huhulia@gmail.com>
2023-09-12 15:47:04 +0200

    added show api

--- app/Http/Controllers/Api/ProjectController.php
+++ app/Http/Controllers/Api/ProjectController.php
@@ -30,7 +30,9 @@ class ProjectController extends Controller
      */
     public function show(string $id)
     {
-        //
+        $project = Project::find($id);
+        if (!$project) return response(null, 404);
+        return response()->json($project);
     }
 
     /**
