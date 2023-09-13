3554113d736f52b70855fa2069e7b0752a01e48f
cloudhuh <claudiu.huhulia@gmail.com>
2023-09-13 19:23:24 +0200

    added contactcontroller & message view & create api

---
+++ app/Http/Controllers/Api/ContactController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Mail\ContactMessageMail;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Mail;
+use Illuminate\Support\Facades\Validator;
+
+class ContactController extends Controller
+{
+    public function message(Request $request)
+    {
+        $data = $request->all();
+        $validator = Validator::make($data, [
+            'email' => 'required|email',
+            'subject' => 'required|string',
+            'message' => 'required|string',
+        ], [
+            'email.required' => 'la mail è obbligatoria',
+            'email.email' => 'la mail non è valida',
+            'subject.required' => 'il soggetto è obbligatorio',
+            'message.required' => 'il contenuto è obbligatorio',
+        ]);
+        if ($validator->fails()) {
+            return response()->json(['errors' =>  $validator->errors()], 400);
+        }
+        $mail = new ContactMessageMail(
+            sender: $data['email'],
+            subject: $data['subject'],
+            content: $data['message']
+        );
+        Mail::to(env('MAIL_TO_ADDRESS'))->send($mail);
+
+        return response(null, 204);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,8 @@
 <?php
 
+use App\Http\Controllers\Api\ContactController;
 use App\Http\Controllers\Api\ProjectController;
+use GuzzleHttp\Psr7\Message;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -18,3 +20,5 @@ use Illuminate\Support\Facades\Route;
 Route::get('/projects', [ProjectController::class, 'index']);
 
 Route::apiResource('/projects', ProjectController::class);
+
+Route::post('/contact-message', [ContactController::class, 'message']);
